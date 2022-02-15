7a5b3cc3806bde2f8a146dfacd6837d9d3e39434
Mridul Gupta <gupta2209mridul@gmail.com>
2022-02-15 22:32:04 +0530

    Day 13

---
+++ 136. Single Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++)
+            count ^= nums[i];
+        return count;
+    }
+}
